3021e3834e8b3a0272ce7f9ce1b171a3d525b549
Flashback054 <37113714+Flashback054@users.noreply.github.com>
2022-11-07 18:24:40 +0700

    client test code and library code
    Added exceptions catch

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
-import clientsocketlib as client
+import testLib as client
+import socket
 PORT=80
 
 # RUN
 
 # Download : Content-Length:
 # client.downloadFile('http://example.com',PORT)
-# client.downloadFile('http://web.stanford.edu/class/cs231a/course_notes/01-camera-models.pdf',PORT)
+# client.downloadFile('http://web.stanford.edu/class/cs231a/course_notes/03-camera-models.pdf',PORT)
 
 # Download : Chunked
 # client.downloadFile('http://www.httpwatch.com/httpgallery/chunked/chunkedimage.aspx',PORT)
@@ -23,8 +24,9 @@ connectionList=[
     ('http://www.httpwatch.com/httpgallery/chunked/chunkedimage.aspx',PORT),
     ('http://www.bing.com',PORT),
     ('http://web.stanford.edu/class/cs231a/course_notes/',PORT),
-    ('http://web.stanford.edu/class/cs143/handouts/',PORT)
+    ('http://web.stanford.edu/class/cs143/handouts/',PORT),
+    ('http://www.google.com/index.html',PORT),
+    ('http://web.stanford.edu/class/cs142/lectures/',PORT)
 ]
 client.multiConnectionDownload(connectionList)
-
 
--- clientsocketlib.py
+++ clientsocketlib.py
@@ -4,18 +4,24 @@ import os
 
 class Header:
     def __init__(self,sock) -> None:
-        self.statusLine=getLine(sock)
-        self.statusCode=int(self.statusLine[self.statusLine.find(' ')+1 : self.statusLine.find(' ')+4])
+        try:
+            self.statusLine=getLine(sock)
+            
+            self.statusCode=int(self.statusLine[self.statusLine.find(' ')+1 : self.statusLine.find(' ')+4])
 
-        while True:
-            line=getLine(sock)
-            if (line=='\r\n'):
-                break
-            else:
-                # ex: Content-Length: 100\r\n ===>headerField='Content-Length', fieldValue='100'
-                headerField=line[0:line.find(':')]
-                fieldValue=line[line.find(': ') + 2 : len(line) - 2]
-                setattr(self,headerField,fieldValue)
+            while True:
+                line=getLine(sock)
+                if (line=='\r\n'):
+                    break
+                else:
+                    # ex: Content-Length: 100\r\n ===>headerField='Content-Length', fieldValue='100'
+                    headerField=line[0:line.find(':')]
+                    fieldValue=line[line.find(': ') + 2 : len(line) - 2]
+                    setattr(self,headerField,fieldValue)
+        except socket.error as err :
+            raise err
+        except Exception as exp:
+            raise exp
 
     def isChunkedEncoding(self):
         if (hasattr(self,'Transfer-Encoding')):
@@ -44,48 +50,69 @@ class Response:
         self.content=""
 
     def getContent(self,sock,header):
-        if (header.isChunkedEncoding()):
-            content=self.getContentFromChunks(sock)
-        else:
-            contentLength=self.header.getContentLength()
-            if (contentLength!=-1):
-                content=self.getContentFromContentLength(sock,contentLength)    
+        try:
+            if (header.isChunkedEncoding()):
+                content=self.getContentFromChunks(sock)
+            else:
+                contentLength=self.header.getContentLength()
+                if (contentLength!=-1):
+                    content=self.getContentFromContentLength(sock,contentLength)    
 
-        return content
+            return content
+        except socket.err as sockErr:
+            raise sockErr
+        except Exception as exp:
+            raise exp
 
     def getContentFromContentLength(self,sock,contentLength):
-        content=recv_s(sock,contentLength)
-        return content
+        try:
+            content=recv_s(sock,contentLength)
+            return content
+        except socket.error as err :
+            raise err
+        except Exception as exp:
+            raise exp
 
     def getContentFromChunks(self,sock):
-        content=[]
-        while True:
-            sizeLine=getLine(sock)
-            # omit '\r\n'
-            if (sizeLine.find(';')!=-1): #chunk extension included
-                sizeLine=sizeLine[0:sizeLine.find(';')].strip()
-            else:
-                sizeLine=sizeLine[0:len(sizeLine)-2].strip()
+        try:
+            content=[]
+            while True:
+                sizeLine=getLine(sock)
+                # omit '\r\n'
+                if (sizeLine.find(';')!=-1): #chunk extension included
+                    sizeLine=sizeLine[0:sizeLine.find(';')].strip()
+                else:
+                    sizeLine=sizeLine[0:len(sizeLine)-2].strip()
 
-            size=int(sizeLine,16)
-            if (size==0):
-                break
-            else:
-                chunk=recv_s(sock,size)
-                content.append(chunk)
-                last=getLine(sock) # discard last \r\n
+                size=int(sizeLine,16)
+                if (size==0):
+                    break
+                else:
+                    chunk=recv_s(sock,size)
+                    content.append(chunk)
+                    last=getLine(sock) # discard last \r\n
 
-        self.getAndDiscardTrailer(sock)
+            self.getAndDiscardTrailer(sock)
 
-        return b"".join(content)
+            return b"".join(content)
+        
+        except socket.error as err :
+            raise err
+        except Exception as exp:
+            raise exp
 
     def getAndDiscardTrailer(self,sock):
-        while True:
-            line=getLine(sock)
-            if (not line):
-                break
-            if (line in ('','\r\n','\n','')):
-                break
+        try :
+            while True:
+                line=getLine(sock)
+                if (not line):
+                    break
+                if (line in ('','\r\n','\n','')):
+                    break
+        except socket.error as err :
+            raise err
+        except Exception as exp:
+            raise exp
 
     def getDecodedContent(self,format):
         return self.content.decode(format)
@@ -115,10 +142,15 @@ class Response:
         return urls
 
 def initConnection(HOST,PORT):
-    hostIP=socket.gethostbyname(HOST)
-    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((hostIP,PORT))
-    return sock
+    try:
+        hostIP=socket.gethostbyname(HOST)
+        sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.connect((hostIP,PORT))
+        return sock
+    except socket.error as err:
+        raise err
+    except socket.gaierror as gaierr:
+        raise gaierr
 
 def splitHostAndPath(HOST):
     # Split url into Host and Path
@@ -136,115 +168,180 @@ def splitHostAndPath(HOST):
     return (host,path)
 
 def sendGETRequest(sock,host,path):
-    request=f'GET /{path} HTTP/1.1\r\nHost: {host}\r\nConnection: Keep-alive\r\n\r\n'
-    sock.sendall(request.encode('utf-8'))
+    try:
+        request=f'GET /{path} HTTP/1.1\r\nHost: {host}\r\nConnection: Keep-alive\r\n\r\n'
+        sock.sendall(request.encode('utf-8'))
+    except socket.error as err :
+        raise err
 
 # safely receive n bytes from socket
 def recv_s(sock,nbytes):
-    remain=nbytes
-    result=[]
-    while True:
-        data=sock.recv(remain)
-        if (not data):
-            return b""
-        remain-=len(data)
-        result.append(data)
-        if (remain==0):
-            break
+    try:
+        remain=nbytes
+        result=[]
+        while True:
+            data=sock.recv(remain)
+            if (len(data)==0): # server has closed the socket
+                raise Exception('Server has closed the socket.')
+            
+            remain-=len(data)
+            result.append(data)
+            if (remain==0):
+                break
 
-    return b"".join(result)
+        return b"".join(result)
+    except socket.error as err:
+        raise err
+    except Exception as exp:
+        raise exp
 
 # Get a line from socket
 def getLine(sock):
-    lines=[]
+    try:
+        lines=[]
 
-    while True:
-        try:
-            chr=recv_s(sock,1)
-
-            if (chr==b""):
-                return ""
-
-            lines.append(chr.decode('utf-8'))
+        while True:
+            chr=recv_s(sock,1)         
+            lines.append(chr)
             # if lines is "...\r\n"
-            if (len(lines)>=2 and lines[len(lines)-2]=='\r' and lines[len(lines)-1]=='\n'):
+            if (len(lines)>=2 and lines[len(lines)-2]==b'\r' and lines[len(lines)-1]==b'\n'):
                 break
-        except socket.error as err:
-            print(f'Socket Error: {err}')
-            return ""
 
-    result="".join(lines)
-    return result
+        result=(b"".join(lines)).decode('utf-8')
+        return result
+    except socket.error as err:
+        raise err
+    except Exception as exp:
+        raise exp
 
 # Write File
 def createFileFromData(path,data):
-    file=open(f'{path}','wb')
-    file.write(data)
-    file.close()
+    try:
+        file=open(f'{path}','wb')
+        file.write(data)
+        file.close()
+    except OSError as oserr:
+        raise oserr
 
 ### MAIN FUNCTION ###
 
 def downloadFile(HOST,PORT): 
-    # split host and path from HOST addr
-    # ex:http://example.com/index.html 
-    # -> host:example.com, path:index.html
-    (host,path)=splitHostAndPath(HOST)
-    
-    sock=initConnection(host,PORT)
-
-    sendGETRequest(sock,host,path)
-
-    # Get and parse response
-    response = Response(sock)
-
-    if (response.header.statusCode==200): 
-        response.content=response.getContent(sock,response.header) # return content in b"" format
-
-        if (path==''):
-            # default: index.html
-            createFileFromData(f'{host}_index.html',response.content)
-        else:
-            fileName=path[path.rfind('/')+1:]
-            createFileFromData(f'{host}_{fileName}',response.content)
-    
-    sock.close()
+    try: 
+        handleDownloadFile(HOST,PORT)
+    except socket.error as sockErr:
+        print(f'Socket error downloading file from {HOST} : {sockErr}')
+    except socket.gaierror as gaiErr:
+        print(f'Get address info error during downloading file from {HOST} : {gaiErr}')
+    except OSError as osErr:
+        print(f'Error writing file downloaded from {HOST} : {osErr}')
+    except Exception as exp:
+        print(f'Exception occurred during downloading file from {HOST} : {exp}')
 
 def downloadAllFiles(HOST,PORT):
-    # split host and path from HOST addr
-    # ex:http://web.stanford.edu/class/cs231a/course_notes/ 
-    # -> host = web.stanford.edu, path = class/cs231a/course_notes/
-    (host,path)=splitHostAndPath(HOST)
-    folderName=path[path.rfind('/',0,len(path)-1)+1 : len(path)-1]
+    try:
+        handleDownloadAllFiles(HOST,PORT)
+    except socket.error as sockErr:
+        print(f'Socket error during downloading all files from folder {HOST} : {sockErr}')
+    except socket.gaierror as gaiErr:
+        print(f'Get address info error during downloading all files from folder {HOST} : {gaiErr}')
+    except OSError as osErr:
+        print(f'Error writing files downloaded from folder {HOST} : {osErr}')
+    except Exception as exp:
+        print(f'Exception occurred during downloading all files from folder {HOST} : {exp}')
 
-    sock=initConnection(host,PORT)
+def handleDownloadFile(HOST,PORT):
+    try:
+        # split host and path from HOST addr
+        # ex:http://example.com/index.html 
+        # -> host:example.com, path:index.html
+        (host,path)=splitHostAndPath(HOST)
+        
+        sock=initConnection(host,PORT)
 
-    sendGETRequest(sock,host,path)
+        sendGETRequest(sock,host,path)
 
-    response = Response(sock)
+        # Get and parse response
+        response = Response(sock)
 
-    if (response.header.statusCode==200 and getattr(response.header,'Connection') == 'Keep-Alive'):
-        response.content=response.getContent(sock,response.header) # return content in b"" format
-        content=response.getDecodedContent('utf-8') # decode in order to parse all <a> tags
+        if (response.header.statusCode >= 200 and response.header.statusCode < 300): 
+            response.content=response.getContent(sock,response.header) # return content in b"" format
 
-        #create folder
-        currDir=os.getcwd()
-        folderPath=f'{host}_{folderName}'
-        os.makedirs(folderPath, exist_ok=True)
+            if (path==''):
+                # default: index.html
+                createFileFromData(f'{host}_index.html',response.content)
+            else:
+                fileName=path[path.rfind('/')+1:]
+                createFileFromData(f'{host}_{fileName}',response.content)
+        else:
+            raise Exception(f'Response status code : {response.header.statusCode} .')
 
-        urls=response.getUrlsFromContent(content,HOST)
-        for url in urls:
-            (urlhost,urlpath)=splitHostAndPath(url)
-            fileName=urlpath[urlpath.rfind('/')+1:]
-            if (fileName!=''): # if fileName=='' ==> url refer to current download folder ==> download forever
-                sendGETRequest(sock,urlhost,urlpath)
+    except socket.error as sockErr:
+        raise sockErr
+    except socket.gaierror as gaiErr:
+        raise gaiErr
+    except OSError as oserr:
+        raise oserr
+    except Exception as exp:
+        raise exp
 
-                subResponse = Response(sock)
+    finally:
+        sock.close()
 
-                if (subResponse.header.statusCode==200):
-                    subResponse.content = subResponse.getContent(sock,subResponse.header)
-                    createFileFromData(os.path.join(currDir,folderPath,fileName),subResponse.content)
 
-    sock.close()
+def handleDownloadAllFiles(HOST,PORT):
+    try:
+        # split host and path from HOST addr
+        # ex:http://web.stanford.edu/class/cs231a/course_notes/ 
+        # -> host = web.stanford.edu, path = class/cs231a/course_notes/
+        (host,path)=splitHostAndPath(HOST)
+        folderName=path[path.rfind('/',0,len(path)-1)+1 : len(path)-1]
+
+        sock=initConnection(host,PORT)
+
+        sendGETRequest(sock,host,path)
+
+        response = Response(sock)
+
+        if (response.header.statusCode>=200 and response.header.statusCode<300):
+            if (getattr(response.header,'Connection') == 'Keep-Alive'):
+                response.content=response.getContent(sock,response.header) # return content in b"" format
+                content=response.getDecodedContent('utf-8') # decode in order to parse all <a> tags
+
+                #create folder
+                currDir=os.getcwd()
+                folderPath=f'{host}_{folderName}'
+                os.makedirs(folderPath, exist_ok=True)
+
+                urls=response.getUrlsFromContent(content,HOST)
+                for url in urls:
+                    (urlhost,urlpath)=splitHostAndPath(url)
+                    fileName=urlpath[urlpath.rfind('/')+1:]
+                    if (fileName!=''): # if fileName=='' ==> url refer to a folder ==> no need to download
+                        sendGETRequest(sock,urlhost,urlpath)
+
+                        subResponse = Response(sock)
+
+                        if (subResponse.header.statusCode==200):
+                            subResponse.content = subResponse.getContent(sock,subResponse.header)
+                            createFileFromData(os.path.join(currDir,folderPath,fileName),subResponse.content)
+            else: # Connection: Close:
+                raise Exception('Server closed connection right after response')
+        else: # statusCode < 200 or statusCode >= 300
+            raise Exception(f'Response status code : {response.header.statusCode} .')
+
+    except socket.error as sockErr:
+        raise sockErr
+    except socket.gaierror as gaiErr:
+        raise gaiErr
+    except OSError as osErr:
+        raise osErr
+    except Exception as exp:
+        raise exp
+
+    finally:
+        sock.close()
+
+
 
 def multiConnectionDownload(connectionList):
     threads=[]
@@ -259,7 +356,7 @@ def multiConnectionDownload(connectionList):
             thread.start()
             threads.append(thread)
     except:
-        print('Error occurred when threading.')
+        print('Error occurred during threading.')
     finally:
         for thread in threads:
             thread.join()
